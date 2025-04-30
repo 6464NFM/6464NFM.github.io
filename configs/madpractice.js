d8251ff2152ffd22d8c033e0b06f287c4d4c532f
D Mack <dmack6464@gmail.com>
2025-04-30 14:10:38 +1000

    Add files via upload

---
+++ configs/madpractice.js
@@ -0,0 +1,14 @@
+var customConfigLoaded = true;
+
+var FOV = 80; //default 55 (will always default to 55 on mobile devices!)
+var HDTextures = true;
+var practiceGame = true;
+var nfm1 = false;
+var carsCanFly = true; //launch your car off a jump and hit shift + spacebar
+var instantRaceStart = true;
+var instantCarSelect = true;
+var requireHandbrakeForStunts = "auto";
+var defaultPowerLevel = 98; //when the game starts, everyone's power level will be this value (default 98)
+var maxPower = 120; //default 98, modified default is 120
+var powerPinnedHigh = 400; //default 200, modified is 400
+var powerPinnedLow = 300; //default 100, modified is 300
