504741f9d47737f42abee3ecd777ce9833aa3e05
D Mack <dmack6464@gmail.com>
2025-04-30 18:58:34 +1000

    Add files via upload

---
+++ nfm3/configs/practice.js
@@ -0,0 +1,14 @@
+var customConfigLoaded = true;
+
+var FOV = 70; //default 55 (will always default to 55 on mobile devices!)
+var HDTextures = true;
+var practiceGame = true;
+var nfm1 = false;
+var carsCanFly = false; //launch your car off a jump and hit shift + spacebar
+var instantRaceStart = true;
+var instantCarSelect = true;
+var requireHandbrakeForStunts = "auto";
+var defaultPowerLevel = 98; //when the game starts, everyone's power level will be this value (default 98)
+var maxPower = 98; //default 98, modified default is 120
+var powerPinnedHigh = 200; //default 200, modified is 400
+var powerPinnedLow = 100; //default 100, modified is 300
